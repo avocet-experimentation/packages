767474256dd9f62ebb4349f40e37b65f4ca82d59
Adnan Shihabi <adnan@ashihabi.me>
2024-10-21 10:50:27 -0700

    unit tests for rest-loader -- testing successful fflag object return and error upon fetching

---
+++ packages/fflags-rest-loader/__tests__/restLoader.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, Mock, test, vi } from "vitest";
+import { RestLoader } from "../src/index.js";
+
+describe("RestLoader", () => {
+  const BASE_URL = "http://localhost:3524/api/fflags/caching";
+
+  global.fetch = vi.fn(); // mocked `fetch`
+
+  // mocking the json function, which returns a promise with the data passed to it
+  const createFetchResponse = (data?: any) => ({
+    json: () => new Promise((resolve) => resolve(data)),
+  });
+
+  const expectedFlags = {
+    flag1: {
+      newFeatureAccess: {
+        enabled: true,
+        value: 100,
+      },
+      oldFeatureAccess: {
+        enabled: false,
+        value: 200,
+      },
+    },
+  };
+
+  // working as expected
+  test("successfully gets the flags from the endpoint", async () => {
+    (fetch as Mock).mockResolvedValue(createFetchResponse(expectedFlags)); // mock expected result when the code calls fetch
+    const loader = new RestLoader(BASE_URL);
+    const fflags = await loader.load("staging");
+    expect(fflags?.get("flag1")).eql(expectedFlags.flag1); // deep compare fflags object
+  });
+
+  // test error
+  test("should fail trying to get the flags (invalid data)", async () => {
+    (fetch as Mock).mockResolvedValue(createFetchResponse());
+    const loader = new RestLoader(BASE_URL);
+    const fflags = await loader.load("staging");
+    expect(fflags).undefined; // should return undefined when an error occurs
+  });
+});
