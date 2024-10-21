8f452e6f709d73fa1c8d39a825aebf3c18ff97e7
Adnan Shihabi <adnan@ashihabi.me>
2024-10-20 21:05:01 -0700

    refactored PORT constant to be configured via .env file

--- fflags-service/src/index.ts
+++ fflags-service/src/index.ts
@@ -2,15 +2,21 @@ import { buildServer } from "./buildServer.js";
 import { connectToDB } from "./connectToDB.js";
 
 const main = async (): Promise<void> => {
-  try {
-    const PORT = 3524; // spells flag on mobile phone keypad xD
-    await connectToDB();
-    const server = await buildServer();
-    await server.listen({ port: PORT });
-    console.log(`FFlag server ready at port ${PORT}`);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+  const PORT = process.env.SERVICE_PORT; // spells flag on mobile phone keypad xD
+  if (typeof PORT === "number") {
+    try {
+      await connectToDB();
+      const server = await buildServer();
+      await server.listen({ port: PORT });
+      console.log(`FFlag server ready at port ${PORT}`);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  } else {
+    console.error(
+      "Could not retrieve port number. Please ensure .env is configure properly"
+    );
   }
 };
 
