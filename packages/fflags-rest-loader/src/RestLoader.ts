984131e4ef49b4614bb55bda8dceb5b6ec699a20
Adnan Shihabi <adnan@ashihabi.me>
2024-10-21 10:43:46 -0700

    fetch flag data using environment name from fflags rest api

---
+++ packages/fflags-rest-loader/src/RestLoader.ts
@@ -0,0 +1,42 @@
+import {
+  EnvironmentName,
+  FeatureFlags,
+  FlagName,
+  UserGroups,
+} from "@fflags/types";
+
+export class RestLoader {
+  private readonly baseUrl: string;
+
+  // the baseUrl is dependent on where the rest service is running
+  // this loader can work with any REST service
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  async load(
+    environmentName: EnvironmentName
+  ): Promise<FeatureFlags | undefined> {
+    try {
+      const response = await fetch(`${this.baseUrl}/${environmentName}`);
+      const fflags = await response.json();
+
+      // if not flag is found, log an error and return `undefined`
+      if (!fflags || Object.keys(fflags).length === 0) {
+        this.handleError(new Error("Empty flags"));
+        return;
+      }
+      const cachedFlags: FeatureFlags = new Map<FlagName, UserGroups>();
+      for (const fflagName in fflags) {
+        cachedFlags.set(fflagName, fflags[fflagName]);
+      }
+      return cachedFlags;
+    } catch (error) {
+      this.handleError(error);
+    }
+  }
+
+  handleError(error: any): void {
+    console.log(`${new Date()}: ${error}`);
+  }
+}
