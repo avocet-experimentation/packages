b1b01fad74dcfe01a06b41e67d799681e7767638
Adnan Shihabi <adnan@ashihabi.me>
2024-10-22 16:47:21 -0700

    Added cors origin for secure communication between client and server

--- fflags-service/src/fflags/fflags.routes.ts
+++ fflags-service/src/fflags/fflags.routes.ts
@@ -1,4 +1,5 @@
-import { FastifyInstance } from "fastify";
+import fastify, { FastifyInstance } from "fastify";
+import cors from "@fastify/cors";
 import {
   createFFlagHandler,
   deleteFFlagHandler,
@@ -7,10 +8,25 @@ import {
   updateFFlagHandler,
 } from "./fflags.controller.js";
 
+// security
+const corsConfig = {
+  origin: (origin, cb) => {
+    const hostname = new URL(origin).hostname;
+    if (hostname === "localhost") {
+      //  Request from localhost will pass
+      cb(null, true);
+      return;
+    }
+    // Generate an error on other origins, disabling access
+    cb(new Error("Not allowed"), false);
+  },
+};
+
 // map http methods to the path and the handlers which are implemented in the controller
 export const getFFlagsRoutes = async (
   server: FastifyInstance
 ): Promise<FastifyInstance> => {
+  await server.register(cors, corsConfig);
   server.post("/", createFFlagHandler); // create new flag, including its environment and respective user groups
   server.put("/:fflagId", updateFFlagHandler); // update entire flag (do we need a patch method?)
   server.delete("/:fflagId", deleteFFlagHandler); // physically remove entire flag
