8e00b70cc0dcfaacea380ade6b1a7ddd81f5e7c1
Adnan Shihabi <adnan@ashihabi.me>
2024-10-22 18:56:48 -0700

    reformatted the types file and http test file

--- packages/fflags-types/src/fflagsTypes.ts
+++ packages/fflags-types/src/fflagsTypes.ts
@@ -1,6 +1,6 @@
 export type FeatureFlagContent = {
-  enabled: boolean
-  value?: unknown // allow for object, string, or number
+  enabled: boolean;
+  value?: unknown; // allow for object, string, or number
 };
 
 export type FlagName = string;
@@ -13,13 +13,15 @@ export type FeatureFlags = Map<FlagName, UserGroups>;
 
 export type EnvironmentName = string;
 
-export type FeatureFlagsLoader = (environmentName: EnvironmentName) => Promise<FeatureFlags>;
+export type FeatureFlagsLoader = (
+  environmentName: EnvironmentName
+) => Promise<FeatureFlags>;
 
 export type FeatureFlagsStartingOptions = {
-  environmentName: EnvironmentName
-  autoRefresh: boolean
-  refreshIntervalInSeconds?: number
-  featureFlagsLoader: FeatureFlagsLoader
+  environmentName: EnvironmentName;
+  autoRefresh: boolean;
+  refreshIntervalInSeconds?: number;
+  featureFlagsLoader: FeatureFlagsLoader;
 };
 
 // eslint-disable-next-line
@@ -29,16 +31,21 @@ type AnyArgs = any[]; // represents any set of arguments passed to a function
 // eslint-disable-next-line
 export type AnyFunction = (...args: AnyArgs) => any; // signature of any function
 
-export type FeatureFunction<Args extends AnyArgs, Result> = (...args: Args) => Result;
+export type FeatureFunction<Args extends AnyArgs, Result> = (
+  ...args: Args
+) => Result;
 
-export type OverrideFuction<F extends AnyFunction> = (flag: FeatureFlagContent, ...args: Parameters<F>) => boolean | Promise<boolean>
+export type OverrideFuction<F extends AnyFunction> = (
+  flag: FeatureFlagContent,
+  ...args: Parameters<F>
+) => boolean | Promise<boolean>;
 
-// contains the informationo we need to choose between two features depending on the flag status
+// contains the information we need to choose between two features depending on the flag status
 // i.e. `on` for enabled, `off` for disabled
 export type FeatureFlagSwitchParams<F extends AnyFunction> = {
-  flagName: FlagName
-  userGroupName: UserGroupName
-  on: FeatureFunction<Parameters<F>, ReturnType<F>>
-  off: FeatureFunction<Parameters<F>, ReturnType<F>>
-  override?: OverrideFuction<F>
-}
+  flagName: FlagName;
+  userGroupName: UserGroupName;
+  on: FeatureFunction<Parameters<F>, ReturnType<F>>;
+  off: FeatureFunction<Parameters<F>, ReturnType<F>>;
+  override?: OverrideFuction<F>;
+};
