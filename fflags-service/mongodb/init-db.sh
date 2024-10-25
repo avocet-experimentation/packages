7ef961cfde896bd7cf286f49c5fcbf750710dda8
Adnan Shihabi <adnan@ashihabi.me>
2024-10-25 12:56:27 -0700

    mongodb docker changes: - removed the volume and instead went with ephemeral storage (for testing purposes)

--- fflags-service/mongodb/init-db.sh
+++ fflags-service/mongodb/init-db.sh
@@ -7,3 +7,6 @@ docker exec -it mongodb-fflags mongosh --quiet \
   --eval 'use fflags' \
   --eval 'db.createUser({ user: "fflags-user", pwd: "1234", roles: [{ role: "readWrite", db: "fflags" }] })' \
   --eval 'show users'
+
+# Command to manually login to Docker mongoDB as authorized user:
+# mongosh --username root --password 1234 --authenticationDatabase admin --quiet
