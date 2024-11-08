04697e8a248428480f6514378cbf7ef61972d505
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-07 17:07:13 -0800

    Fixed circular schema dependency
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/index.ts
+++ estuary-types/src/index.ts
@@ -1,20 +1,8 @@
 export * from './attributes.js';
-// export * from './attributes.types.js';
-
 export * from './environments.js';
-// export * from './environments.types.js';
-
-export * from './fflags.js';
-// export * from './fflags.types.js';
-
+export * from './experiments.js';
+export * from './featureFlags.js';
 export * from './general.js';
-// export * from './general.types.js';
-
 export * from './overrideRules.js';
-// export * from './overrideRules.types.js';
-
 export * from './telemetry.js';
-// export * from './telemetry.types.js';
-
 export * from './users.js';
-// export * from './users.types.js';
