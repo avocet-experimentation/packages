b15dbb13751a2314f5479184cb711bb4bf1a707b
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-15 12:31:30 -0800

    Add environment property to OverrideRule indicating what environment it applies to; exported ClientPropValue for external use
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/overrideRules.ts
+++ estuary-types/src/overrideRules.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { bsonObjectIdHexStringSchema, proportionSchema } from "./util.js";
 import { clientPropNameSchema } from "./flagClients.js";
+import { environmentNameSchema } from "./environments.js";
 
 export const ruleTypeSchema = z.enum(["Experiment", "ForcedValue"]);
 export type RuleType = z.infer<typeof ruleTypeSchema>;
@@ -13,19 +14,24 @@ export const ruleStatusSchema = z.enum([
 ]);
 export type RuleStatus = z.infer<typeof ruleStatusSchema>;
 
+export const enrollmentSchema = z.object({
+  attributes: z.array(clientPropNameSchema),
+  proportion: proportionSchema,
+});
+
+export interface Enrollment extends z.infer<typeof enrollmentSchema> {};
+
 export const overrideRuleSchema = z.object({
   type: ruleTypeSchema,
   status: ruleStatusSchema,
   description: z.string().optional(),
+  environment: environmentNameSchema,
   startTimestamp: z.number().int().gte(0).optional(),
   endTimestamp: z.number().int().gte(0).optional(),
-  enrollment: z.object({
-    attributes: z.array(clientPropNameSchema),
-    proportion: proportionSchema,
-  }),
+  enrollment: enrollmentSchema,
 });
 /**
  * Any rule that causes a flag value to differ from its default, including
  *  experiments and values forced per environment
  */
-export interface OverrideRule extends z.infer<typeof overrideRuleSchema> {}
+export interface OverrideRule extends z.infer<typeof overrideRuleSchema> {};
