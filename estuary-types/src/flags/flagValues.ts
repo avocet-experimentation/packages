58fa86bf2a048c5f7013443876514dd6c70dc19a
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-15 12:58:02 -0800

    Renamed the default value field on feature flags to 'initial' to avoid issues where default is a reserved keyword
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/flags/flagValues.ts
+++ estuary-types/src/flags/flagValues.ts
@@ -8,15 +8,15 @@ export type FlagCurrentValue = z.infer<typeof flagCurrentValueSchema>;
 
 export const flagBooleanValueSchema = z.object({
   type: z.literal("boolean"),
-  default: z.boolean(),
+  initial: z.boolean(),
 });
 export const flagStringValueSchema = z.object({
   type: z.literal("string"),
-  default: z.string(),
+  initial: z.string(),
 });
 export const flagNumberValueSchema = z.object({
   type: z.literal("number"),
-  default: z.number(),
+  initial: z.number(),
 });
 
 export const flagValueDefSchema = z.union([
