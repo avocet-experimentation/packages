d9092bbceb95c5458f7bf00e16a0fcc819e2772f
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-19 15:37:06 -0800

    Added error classes for MongoRepository
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ estuary-types/src/classes/errors.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { EstuaryMongoTypes } from '../general.js';
+import { GeneralRecord } from "../util.js";
+
+export class SchemaParseError<T extends EstuaryMongoTypes> extends Error {
+  constructor(safeParseError: z.SafeParseError<T>) {
+    const formattedError = safeParseError.error.format();
+    console.log('schema parse errors:', formattedError);
+    super(formattedError.toString());
+    this.name = 'Schema ParseError';
+  }
+}
+
+export class DocumentNotFoundError extends Error {
+  constructor(query: GeneralRecord) {
+    super(`Document not found matching ${JSON.stringify(query)}`);
+    this.name = 'DocumentNotFoundError';
+  }
+}
+
+export class DocumentUpdateFailedError extends Error {
+  constructor(id: string) {
+    super(`Failed to modify document with id ${id}`);
+    this.name = 'DocumentUpdateFailedError';
+  }
+}
