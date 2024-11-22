d49f4442e501ee1e32ae76e53fc94993811c55de
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-22 14:15:15 -0800

    `FeatureFlagDraft` changes: - Moved override rules to a top-level property - Added an `environmentIds` array to the top level representing all environments the flag is enabled in - Removed now-unnecessary `environments` property
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/feature-flags/classes.ts
+++ estuary-types/src/feature-flags/classes.ts
@@ -1,36 +1,34 @@
 import { z } from "zod";
 import { featureFlagDraftSchema, OverrideRuleUnion } from "./schema.js";
-import {
-  FlagEnvironmentMappingTemplate,
-  FlagEnvironmentPropsTemplate,
-  FlagEnvironmentMapping,
-  FlagValueDefImpl,
- } from "./child-classes.js";
+import { FlagValueDefImpl } from "./child-classes.js";
 import { FeatureFlag } from "../shared/imputed.js";
-import { FlagValueDef, FlagValueFromTypeDef, FlagValueTypeDef } from "../helpers/flag-value.js";
+import { FlagValueFromTypeDef, FlagValueTypeDef } from "../helpers/flag-value.js";
 import { RequireOnly } from "../helpers/utility-types.js";
 
-type DraftRequired<T extends FlagValueTypeDef> = RequireOnly<FeatureFlagDraft<T>, 'name' | 'value'>;
 
 export class FeatureFlagDraft<T extends FlagValueTypeDef = FlagValueTypeDef> implements z.infer<typeof featureFlagDraftSchema> {
   name: string;
   description: string | null;
   value: FlagValueFromTypeDef<T>;
-  environments: FlagEnvironmentMapping;
+  environmentIds: string[];
+  overrideRules: OverrideRuleUnion[];
 
   constructor(featureFlagDraft: FeatureFlagDraft<T>) {
     this.name = featureFlagDraft.name;
     this.description = featureFlagDraft.description;
     this.value = featureFlagDraft.value;
-    this.environments = featureFlagDraft.environments;
+    this.environmentIds = featureFlagDraft.environmentIds;
+    this.overrideRules = featureFlagDraft.overrideRules;
   }
 
   static template<T extends FlagValueTypeDef>(
-    partialFlagDraft: DraftRequired<T> & { value: FlagValueDefImpl<T> },
+    partialFlagDraft: RequireOnly<FeatureFlagDraft<T>, 'name' | 'value'> 
+      & { value: FlagValueDefImpl<T> },
   ) {
     const defaults = {
       description: null,
-      environments: new FlagEnvironmentMappingTemplate(),
+      environmentIds: [],
+      overrideRules: [],
     };
 
     return new FeatureFlagDraft({ ...defaults, ...partialFlagDraft });
@@ -38,61 +36,13 @@ export class FeatureFlagDraft<T extends FlagValueTypeDef = FlagValueTypeDef> imp
 
   /* Helpers for working with FeatureFlags */
   /**
-   * Get the data for a given environment on a draft or completed feature flag
+   * Get all rules on a flag corresponding to the passed environment name
    */
-  static environmentData<T extends FlagValueTypeDef>(flag: FeatureFlagDraft<T> | FeatureFlag, environmentName: string) {
-    if (!(environmentName in flag.environments)) {
-      Object.assign(
-        flag.environments, {
-        [environmentName]: new FlagEnvironmentPropsTemplate({ name: environmentName }),
-      });
-    }
-
-    return flag.environments[environmentName];
-  }
-
-  /**
-   * Get all rules on a flag corresponding to the passed environment name, or 
-   * all its rules if none are passed
-   */
-  static getRules<T extends FlagValueTypeDef>(
-    flag: FeatureFlagDraft<T> | FeatureFlag,
-    environmentName?: string,
+  static getEnvironmentRules(
+    flag: FeatureFlagDraft | FeatureFlag,
+    environmentName: string,
   ) {
-    if (!environmentName) {
-      const collatedRules = Object.values(flag.environments)
-        .reduce(
-          (acc, { overrideRules }) => acc.concat(overrideRules),
-          [] as OverrideRuleUnion[],
-        );
-      return collatedRules;
-    } else if (isKeyOf(environmentName, flag.environments)) {
-      return flag.environments[environmentName].overrideRules;
-    } else {
-      return [];
-    }
+      return flag.overrideRules
+        .filter((rule) => rule.environmentName === environmentName);
   }
 }
-
-function isKeyOf<T extends Record<any, any>>(
-  possibleKey: string | number | symbol,
-  obj: T,
-): possibleKey is keyof T {
-  return (possibleKey in obj 
-    && (
-      typeof possibleKey !== 'symbol' && obj[possibleKey] !== undefined
-      || obj.possibleKey !== undefined
-    )
-  );
-}
-
-// export class FeatureFlagDraftTemplate extends FeatureFlagDraft {
-//   constructor(partialFlagDraft: RequireOnly<FeatureFlagDraft, 'name' | 'value'>) {
-//     const defaults = {
-//       description: null,
-//       environments: new FlagEnvironmentMappingTemplate(),
-//     };
-
-//     super({ ...defaults, ...partialFlagDraft });
-//   }
-// }
--- estuary-types/src/helpers/utility-functions.ts
+++ estuary-types/src/helpers/utility-functions.ts
@@ -4,3 +4,16 @@ export const idMap = <T extends { id: string; }>(
 ): Record<string, T> => records.reduce((acc, el) => {
   return Object.assign(acc, { [el.id]: el });
 }, {} as Record<string, T>);
+
+
+export function isKeyOf<T extends Record<any, any>>(
+  possibleKey: string | number | symbol,
+  obj: T,
+): possibleKey is keyof T {
+  return (possibleKey in obj 
+    && (
+      typeof possibleKey !== 'symbol' && obj[possibleKey] !== undefined
+      || obj.possibleKey !== undefined
+    )
+  );
+}
