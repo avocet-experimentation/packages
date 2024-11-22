cf7d2a37ceea51a581c2da767defb60a82f7abc9
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-22 12:26:01 -0800

    Fixed discriminated union on `FeatureFlagDraft.value`
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/feature-flags/child-classes.ts
+++ estuary-types/src/feature-flags/child-classes.ts
@@ -2,29 +2,53 @@ import { z } from "zod";
 import {
   OverrideRuleUnion,
   flagEnvironmentPropsSchema,
-  flagEnvironmentMappingSchema
+  flagEnvironmentMappingSchema,
 } from "./schema.js";
-import { FlagValueTypeDef, FlagCurrentValue, FlagValueDef } from "../helpers/flag-value.js";
+import {
+  FlagValueTypeDef,
+  FlagCurrentValue,
+  FlagValueDef,
+  flagBooleanValueSchema,
+  flagNumberValueSchema,
+  flagStringValueSchema,
+  FlagCurrentValueFromTypeDef,
+} from "../helpers/flag-value.js";
 import { RequireOnly } from "../helpers/utility-types.js";
 
+export class FlagValueDefImpl<T extends FlagValueTypeDef> {
+  type: T;
+  initial: FlagCurrentValueFromTypeDef<T>;
 
-class FlagValueDefImpl {
-  type: FlagValueTypeDef;
-  initial: FlagCurrentValue;
+  constructor(flagValueDef: FlagValueDefImpl<T>) {
+    this.type = flagValueDef.type;
+    this.initial = flagValueDef.initial;
+  }
 
-  constructor({ type, initial }: FlagValueDef) {
-    this.type = type;
-    this.initial = initial;
+  static template<T extends FlagValueTypeDef>(type: T) {
+    if (type === 'boolean') {
+      return new FlagValueDefImpl({ type: 'boolean', initial: false });
+    } else if (type === 'string') {
+      return new FlagValueDefImpl({ type: 'string', initial: '' });
+    } else if (type === 'number') {
+      return new FlagValueDefImpl({ type: 'number', initial: 0 });
+    } else {
+      throw new TypeError(`Argument ${type} must be a FlagValueTypeDef`);
+    }
+  }
+
+  static templateBoolean() {
+    return new FlagValueDefImpl({ type: 'boolean', initial: false });
   }
 }
-export class FlagValueDefTemplate extends FlagValueDefImpl {
-  constructor(type: FlagValueTypeDef) {
-    let defaults: FlagValueDef = { type: 'boolean' as const, initial: false };
-    if (type === 'string') defaults = { type: 'string' as const, initial: '' };
-    else if (type === 'number') defaults = { type: 'number' as const, initial: 0 };
-    super(defaults);
-  }
-}
+// export class FlagValueDefTemplate<T extends FlagValueDef> extends FlagValueDefImpl<T> {
+//   constructor(type: T['type']) {
+//     let defaults: FlagValueDef = { type: 'boolean' as const, initial: false };
+//     // if (type === '')
+//     if (type === 'string') defaults = { type: 'string' as const, initial: '' };
+//     else if (type === 'number') defaults = { type: 'number' as const, initial: 0 };
+//     super(defaults);
+//   }
+// }
 /**
  * Environment-specific data for a `FeatureFlag`
  */
