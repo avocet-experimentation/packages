d9be676ccd26759bd4539feff7c22d5f25f2fee9
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-27 14:45:44 -0800

    New functions: - to assert or narrow types to objects - to remove all keys containing `undefined` values from objects
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/helpers/utility-functions.ts
+++ estuary-types/src/helpers/utility-functions.ts
@@ -1,3 +1,4 @@
+import { GeneralRecord } from "./utility-types.js";
 
 export const idMap = <T extends { id: string; }>(
   records: T[]
@@ -16,4 +17,47 @@ export function isKeyOf<T extends Record<any, any>>(
       || obj.possibleKey !== undefined
     )
   );
+}
+
+function isObject(arg: unknown): arg is GeneralRecord {
+  const passedChecks = typeof arg === "object"
+    && arg !== null
+    && !Array.isArray(arg);
+  return passedChecks;
+}
+
+export function assertObject(arg: unknown): asserts arg is GeneralRecord {
+  if (!isObject(arg)) {
+    throw new TypeError(`Argument ${JSON.stringify(arg)} is not an object!`);
+  }
+}
+
+export function isObjectWithProps(arg: unknown): arg is GeneralRecord {
+  const passedChecks = isObject(arg) && Object.keys(arg)
+    .filter(((key) => typeof key !== 'symbol')).length >= 1;
+  return passedChecks;
+}
+/**
+ * Returns a copy of the input object with any keys containing `undefined`
+ *  removed. Throws if the input isn't an object.
+ */
+export function stripKeysWithUndefined(input: object) {
+  assertObject(input);
+
+  const rec = (obj: GeneralRecord) => {
+    const result = { ...obj };
+  
+    Object.keys(result).forEach((key) => {
+      const value = obj[key];
+      if (result[key] === undefined) {
+        delete result[key];
+      } else if (isObjectWithProps(value)) {
+        result[key] = rec(value);
+      }
+    });
+  
+    return result;
+  }
+
+  return rec(input);
 }
