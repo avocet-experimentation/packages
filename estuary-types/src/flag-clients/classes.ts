8c2bdf15e8fc6b028e42cb8507b763a9c8655f90
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-10 01:34:25 -0800

    Add ClientConnectionDraft class and tentative API key generating function
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- estuary-types/src/flag-clients/classes.ts
+++ estuary-types/src/flag-clients/classes.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
+import crypto from 'crypto';
 import { clientPropDefDraftSchema } from './client-props.schema.js';
 import { FlagValueTypeDef } from '../feature-flags/flag-value.js';
 import { RequireOnly } from '../helpers/utility-types.js';
+import { clientConnectionDraftSchema } from './client-connections.schema.js';
 
 /**
  * Definition of a client property visible server-side
@@ -32,5 +34,43 @@ implements z.infer<typeof clientPropDefDraftSchema> {
     };
 
     return new ClientPropDefDraft({ ...defaults, ...partialDraft });
+  }
+}
+
+export class ClientConnectionDraft
+implements z.infer<typeof clientConnectionDraftSchema> {
+  name: string;
+
+  description: string | null;
+
+  environmentId: string;
+
+  clientKeyHash: string;
+
+  constructor(draft: ClientConnectionDraft) {
+    this.name = draft.name;
+    this.description = draft.description;
+    this.environmentId = draft.environmentId;
+    this.clientKeyHash = draft.clientKeyHash;
+  }
+
+  static template(
+    partialDraft: RequireOnly<ClientConnectionDraft, 'name' | 'environmentId'>,
+  ) {
+    const defaults = {
+      description: null,
+      clientKeyHash: this.generateApiKey(),
+    };
+
+    return new ClientConnectionDraft({ ...defaults, ...partialDraft });
+  }
+
+  /**
+   * Generates an API key.
+   * @param length The number of bytes for the API key (default is 32 bytes).
+   * @returns A base64-encoded API key string.
+   */
+  static generateApiKey(length = 32) {
+    return crypto.randomBytes(length).toString('base64').replace(/[=+/]/g, '');
   }
 }
