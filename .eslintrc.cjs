dd4484c86977c60fc8a88ed5556cb7471d992392
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-15 12:22:34 -0800

    Ignore unused vars prefixed with `_`
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2020: true,
+    es2020: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'airbnb-base',
-    'airbnb-typescript/base',
+    'airbnb-typescript/base'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -16,17 +16,29 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json'],
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   rules: {
     'no-unused-vars': 'off', // handled by TS
     'implicit-arrow-linebreak': 'off',
     'import/prefer-default-export': 'off',
     'max-classes-per-file': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true
+      }
+    ]
   },
   settings: {
     'import/resolver': {
-      typescript: {},
-    },
-  },
+      typescript: {}
+    }
+  }
 };
