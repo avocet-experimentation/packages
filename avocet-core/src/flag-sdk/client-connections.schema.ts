348c1cdeb8a967a47e62ec8c4f3aa0a6472a67d8
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-19 12:56:03 -0800

    `SDKConnectionDraft`: Add field to store list of allowed origins
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- avocet-core/src/flag-sdk/client-connections.schema.ts
+++ avocet-core/src/flag-sdk/client-connections.schema.ts
@@ -2,11 +2,12 @@ import { z } from 'zod';
 import { nonEmptyStringSchema } from '../helpers/bounded-primitives.js';
 
 /**
- * For client app connections to the cattails feature flagging service
+ * For applications to connect to the feature flagging API
  */
 export const sdkConnectionDraftSchema = z.object({
   name: nonEmptyStringSchema,
   description: z.string().nullable(),
   environmentId: z.string(),
   clientKeyHash: z.string(),
+  allowedOrigins: z.array(z.string()),
 });
