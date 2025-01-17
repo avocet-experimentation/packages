23cfe3057db51594d8bda3dc3a1abc2a9c5ab342
Gautam Panakkal <gautampanakkal@gmail.com>
2025-01-17 15:45:59 -0800

    SDK: - Add `useStale` option - Replace handwritten cache with `quick-lru` - Add `getLive` method to fetch live flag values asynchronously - Add `fetchFlag` method to update individual flag - Reorganize for readability
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- avocet-sdk/src/clientTypes.ts
+++ avocet-sdk/src/clientTypes.ts
@@ -1,15 +1,20 @@
-import { ClientPropMapping } from '@avocet/core';
+import { ClientPropMapping, ClientSDKFlagValue } from '@avocet/core';
+import { Options } from 'quick-lru';
 
 export type ClientOptions = {
-  apiUrl: string;
   apiKey: string;
+  apiUrl: string;
   autoRefresh: boolean;
-  clientProps: ClientPropMapping;
-  refreshIntervalInSeconds?: number;
   attributeAssignmentCb?: (
     attributes: Record<string, string>,
     ...args: unknown[]
   ) => void;
+  /** set `maxSize` greater than or equal to the number of flags on the environment */
+  cacheOptions: Options<string, ClientSDKFlagValue>;
+  clientProps: ClientPropMapping;
+  refreshIntervalInSeconds?: number;
+  /** Whether to keep or discard flag values when fetching. Default `true` */
+  useStale?: boolean;
 };
 
 const attributeCategoryPrefixes = ['feature-flag', 'client-prop'] as const;
