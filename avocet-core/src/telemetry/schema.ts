e4f2d66e4368dd677f118d2c25cd7c71658a3eff
Adnan Shihabi <adnan@ashihabi.me>
2025-01-17 15:56:58 -0800

    Schema: Create TransformedSpan Schema and Class
    Co-authored-by: Gautam Panakkal 111669555+gpanakkal@users.noreply.github.com Co-authored-by: Sean Mentele 56010359+SeanM0124@users.noreply.github.com

--- avocet-core/src/telemetry/schema.ts
+++ avocet-core/src/telemetry/schema.ts
@@ -1,23 +1,60 @@
-import { PrimitiveTypeLabel } from '../helpers/bounded-primitives.js';
+import { z } from 'zod';
+import {
+  PrimitiveTypeLabel,
+  primitiveTypeLabelSchema,
+} from '../helpers/bounded-primitives.js';
 import { TextPrimitive } from './otel/attributes.js';
 
 export type TransformedSpanAttributes = {
   [key: string]: { type: PrimitiveTypeLabel; value: string };
 };
+
+export const transformedSpanSchema = z.object({
+  traceId: z.string(),
+  spanId: z.string(),
+  parentSpanId: z.string(),
+  name: z.string(),
+  kind: z.number(),
+  startTimeUnixNano: z.string(),
+  endTimeUnixNano: z.string(),
+  attributes: z.record(
+    z.string(),
+    z.object({ type: primitiveTypeLabelSchema, value: z.string() }),
+  ),
+});
+
 /**
  * Telemetry data structure expected by dashboard.
  * Based on OpenTelemetry's span structure, with transformations
  * to enable CQL searches by attribute name
  */
-export interface TransformedSpan {
+export class TransformedSpan implements z.infer<typeof transformedSpanSchema> {
   traceId: string;
+
   spanId: string;
+
   parentSpanId: string;
+
   name: string;
+
   kind: number;
+
   startTimeUnixNano: string;
+
   endTimeUnixNano: string;
+
   attributes: TransformedSpanAttributes;
+
+  constructor(arg: TransformedSpan) {
+    this.traceId = arg.traceId;
+    this.spanId = arg.spanId;
+    this.parentSpanId = arg.parentSpanId;
+    this.name = arg.name;
+    this.kind = arg.kind;
+    this.startTimeUnixNano = arg.startTimeUnixNano;
+    this.endTimeUnixNano = arg.endTimeUnixNano;
+    this.attributes = arg.attributes;
+  }
 }
 
 export type CoercedSpanAttributes = { [x: string]: TextPrimitive };
