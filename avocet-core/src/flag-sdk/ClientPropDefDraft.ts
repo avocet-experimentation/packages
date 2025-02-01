59713b6e8a04373b09c990f96eea493bb66b0fae
Gautam Panakkal <gautampanakkal@gmail.com>
2025-01-31 16:47:08 -0800

    Split `SDKConnectionDraft` and `ClientPropDefDraft` into separate files for clarity
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ avocet-core/src/flag-sdk/ClientPropDefDraft.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+import { clientPropDefDraftSchema } from './client-props.schema.js';
+import { FlagValueTypeDef } from '../feature-flags/flag-value.js';
+import { RequireOnly } from '../helpers/utility-types.js';
+
+/**
+ * Definition of a client property visible server-side
+ */
+export class ClientPropDefDraft
+implements z.infer<typeof clientPropDefDraftSchema> {
+  name: string;
+
+  description: string | null;
+
+  dataType: FlagValueTypeDef;
+
+  isIdentifier: boolean;
+
+  constructor(draft: ClientPropDefDraft) {
+    this.name = draft.name;
+    this.description = draft.description;
+    this.dataType = draft.dataType;
+    this.isIdentifier = draft.isIdentifier;
+  }
+
+  static template(
+    partialDraft: RequireOnly<ClientPropDefDraft, 'name' | 'dataType'>,
+  ) {
+    const defaults = {
+      description: null,
+      isIdentifier: false,
+    };
+
+    return new ClientPropDefDraft({ ...defaults, ...partialDraft });
+  }
+}
--- avocet-core/src/flag-sdk/SDKConnectionDraft.ts
+++ avocet-core/src/flag-sdk/SDKConnectionDraft.ts
@@ -1,40 +1,6 @@
 import { z } from 'zod';
-import { clientPropDefDraftSchema } from './client-props.schema.js';
-import { FlagValueTypeDef } from '../feature-flags/flag-value.js';
 import { RequireOnly } from '../helpers/utility-types.js';
 import { sdkConnectionDraftSchema } from './client-connections.schema.js';
-
-/**
- * Definition of a client property visible server-side
- */
-export class ClientPropDefDraft
-implements z.infer<typeof clientPropDefDraftSchema> {
-  name: string;
-
-  description: string | null;
-
-  dataType: FlagValueTypeDef;
-
-  isIdentifier: boolean;
-
-  constructor(draft: ClientPropDefDraft) {
-    this.name = draft.name;
-    this.description = draft.description;
-    this.dataType = draft.dataType;
-    this.isIdentifier = draft.isIdentifier;
-  }
-
-  static template(
-    partialDraft: RequireOnly<ClientPropDefDraft, 'name' | 'dataType'>,
-  ) {
-    const defaults = {
-      description: null,
-      isIdentifier: false,
-    };
-
-    return new ClientPropDefDraft({ ...defaults, ...partialDraft });
-  }
-}
 
 export class SDKConnectionDraft
 implements z.infer<typeof sdkConnectionDraftSchema> {
@@ -73,7 +39,6 @@ implements z.infer<typeof sdkConnectionDraftSchema> {
    * @param length The number of bytes for the API key (default is 32 bytes).
    * @returns A base64-encoded API key string.
    */
-
   static generateApiKey(length = 32) {
     const randomBytes = globalThis.crypto.getRandomValues(
       new Uint8Array(length),
